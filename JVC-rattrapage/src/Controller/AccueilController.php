3816c11904497c742d86be445fc491719d3ac958
Caropox <54894433+Caropox@users.noreply.github.com>
2020-07-17 15:20:14 +0200

    création accueilController + début

---
+++ JVC-rattrapage/src/Controller/AccueilController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+Class AccueilController extends AbstractController
+{
+    /**
+     * @Route("accueilJVC", name="accueil")
+     */
+
+
+     public function accueil()
+    {
+            return
+    }
+
+
+
+}
+?>

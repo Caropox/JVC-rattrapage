e28d96559c1a6b313e5aa8d85573f80f9a395b95
Caropox <54894433+Caropox@users.noreply.github.com>
2020-08-12 14:50:04 +0200

    creation ConnexionController + styleConnexion.css + connexion.html.twig

---
+++ JVC-rattrapage/src/Controller/ConnexionController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+Class ConnexionController extends AbstractController
+{
+    /**
+     * @Route("/", name="Connexion_JVC")
+     */
+
+
+     public function accueil()
+    {
+        return $this->render('connexion.html.twig');
+    }
+
+
+}
+?>
